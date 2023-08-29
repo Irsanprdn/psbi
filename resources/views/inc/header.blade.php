c19ccb9449387fd119fae9b14cb313653364a981
Irsanprdn <muhammadirsanperdana@gmail.com>
2023-08-30 01:39:07 +0700

    update

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,12 +1,12 @@
 <header id="header" class="fixed-top d-flex align-items-center py-3 px-3">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="logo mb-2">
                     <a href="{{ route('compro') }}"><img src="{{ asset('assets') }}/compro/img/logonavbar.png" alt="" class="img-fluid"></a>
                 </div>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <nav id="navbar" class="navbar">
                     <div class="form-group search">
                         <span class="bi bi-search form-control-feedback" onclick="typeToSearch()"></span>
@@ -14,6 +14,7 @@
                     </div>
 
                     <ul>
+                        <li><a onclick="typeToSearch()" class="nav-link scrollto cursor-pointer">Terbaru</a></li>
                         <li><a onclick="menujuMenu()" class="nav-link scrollto active" href="{{ asset('assets') }}/compro/#hero">Beranda</a></li>
                         <li><a onclick="menujuMenu()" class="nav-link scrollto" href="{{ asset('assets') }}/compro/#about">Tentang Kami</a></li>
                         <li><a onclick="menujuMenu()" class="nav-link scrollto" href="{{ asset('assets') }}/compro/#activity">Aktifitas</a></li>
